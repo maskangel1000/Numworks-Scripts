0af74061af188c7344b45a73f3d5396aaa117065
maskangel1000 <110355218+maskangel1000@users.noreply.github.com>
2023-10-24 15:19:50 -0500

    Add docstring comments

--- image_lib/image_lib.py
+++ image_lib/image_lib.py
@@ -1,21 +1,56 @@
 import kandinsky
 
-""""""
-class Data:
+class Calculator:
+  """A dataclass to store data for the calculator
+
+  width : int = 320
+    the width of the calculator screen
+  height : int = 200
+    the height of the calculator screen
+  fps : int = 30
+    the frames drawn to the screen per second
+  """
   width: int = 320
   height: int = 200
   fps: int = 30
 
-""""""
 class Image:
+  """A class to represent an image by an array of pixels
+
+  Attributes
+    pixels : list[list[list[int]]]
+      2D array of [int, int, int] arrays representing the color of a pixel
+    x : int
+      The top left x coordinate of the image
+    y : int
+      The top left y coordinate of the image
+    is_drawing : bool
+      If the image should be drawn in the next frame
+  """
   
-  """"""
   def __init__(self,
       pixels: list[list[list[int]]],
       x: int,
       y: int,
       is_drawing: bool = False
       ):
+    """Constructs the Image object
+
+    Parameters
+      pixels : list[list[list[int]]]
+        2D array of [int, int, int] arrays representing the color of a pixel
+      x : int
+        The top left x coordinate of the image
+      y : int
+        The top left y coordinate of the image
+      is_drawing : bool = False
+        If the image should be drawn in the next frame
+
+    Raises
+      TypeError
+        If the image dimensions are not uniform
+    """
+        
     # Exception handling
     row_length = len(pixels[0])
     for row in pixels:
@@ -29,8 +64,8 @@ class Image:
     self.height = len(pixels)
     self.is_drawing = is_drawing
 
-  """"""
-  def draw(self):
+  def draw(self) -> None:
+    """Draws the image to the screen if is_drawing is True"""
     # Do not draw if is_drawing is False
     if not self.is_drawing:
       return
@@ -41,26 +76,39 @@ class Image:
         pixel = kandinsky.color(self.pixels[y][x])
         kandinsky.set_pixel(x, y, pixel)
 
-  """"""
-  def center(self):
+  def center(self) -> None:
+    """Moves the x and y of the image to the center of the screen"""
     # Set x and y to center
-    self.x = Data.width // 2 - self.width // 2
-    self.y = Data.height // 2 - self.height // 2
+    self.x = Calculator.width // 2 - self.width // 2
+    self.y = Calculator.height // 2 - self.height // 2
 
-""""""
 class Screen:
+  """A class to represent what is drawn to the screen
 
-  """"""
+  Attributes
+    images : list[Image]
+      An array of the images that are drawn to the screen
+    is_drawing : bool
+      If the images should be drawn in the next frame
+  """
   def __init__(self,
       images: list[Image] = [],
       is_drawing: bool = False
       ):
+    """Constructs a Screen object
+
+    Parameters
+      images : list[Image] = []
+        An array of the images that are drawn to the screen
+      is_drawing : bool = False
+        If the images should be drawn in the next frame
+    """
     # Assign values
     self.images = images
     self.is_drawing = is_drawing
 
-  """"""
-  def draw(self):
+  def draw(self) -> None:
+    """Draws the images to the screen if is_drawing is True"""
     # Do not draw if is_drawing is False
     if not self.is_drawing:
       return
@@ -68,17 +116,27 @@ class Screen:
     for image in self.images:
       image.draw()
 
-  """"""
   def add_image(self,
       image: Image
-      ):
+      ) -> None:
+    """Adds an Image object to the images array
+
+    Parameters
+      image
+        The Image object to be added to the images array
+    """
     # Add image to images array
     self.images.append(image)
 
-  """"""
   def remove_image(self,
       image: Image
-      ):
+      ) -> None:
+    """Removes an Image object from the images array
+
+    Parameters
+      image
+        The Image object to be removed from the images array
+    """
     # Check for image in images array
     for idx, img in enumerate(this.images):
       if img is image:
