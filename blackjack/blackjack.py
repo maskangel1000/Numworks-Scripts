2ddc336ecb8b3765c6a2718f3d6f86970dc8ce1b
maskangel1000 <110355218+maskangel1000@users.noreply.github.com>
2023-11-15 12:47:26 -0600

    blackjack

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,80 +1,152 @@
+import kandinsky
+import ion
 import random
+
+HEIGHT = 220
 
 class Card:
   
-  def __init__(self, value=None, suit=None):
+  CARD_OUTSIDE_WIDTH = 50
+  CARD_OUTSIDE_HEIGHT = 80
+  CARD_OUTSIDE_COLOR = kandinsky.color(0, 0, 0)
+  CARD_INSIDE_WIDTH = 40
+  CARD_INSIDE_HEIGHT = 70
+  CARD_INSIDE_COLOR = kandinsky.color(255, 255, 255)
+  
+  def __init__(self, value: int, suit: str):
     self.value = value
     self.suit = suit
-    if self.value == None:
-      self.value = Card.generate_random_value()
-    if self.suit == None:
-      self.suit = Card.generate_random_suit()
-  
-  @staticmethod
-  def generate_random_value():
-    return random.randint(1, 13)
-  
-  @staticmethod
-  def generate_random_suit():
-    return random.choice(["Spades", "Clubs", "Hearts", "Diamonds"])
 
-  def __str__(self):
-    value = self.value
-    if self.value == 11:
-      value = "Jack"
+  def __str__(self) -> str:
+    return self.get_value_str() + " of " + self.suit
+
+  def get_value_str(self) -> str:
+    if self.value == 1:
+      value = "A"
+    elif self.value == 11:
+      value = "J"
     elif self.value == 12:
-      value = "Queen"
+      value = "Q"
     elif self.value == 13:
-      value = "King"
-    return str(str(value) + " of " + self.suit)
-    
-class Game:
+      value = "K"
+    else:
+      value = str(self.value)
+    return value
   
-  def __init__(self, deck=None):
-    print("assigning deck")
-    self.deck = deck
-    if self.deck == None:
-      print("deck is none, generating deck")
-      self.deck = Game.generate_random_deck()
-      print("random deck generated")
-  
-  @staticmethod
-  def generate_random_deck():
-    deck = []
-    for i in range(52):
-      print("generating new card")
-      new_card = Card()
-      unique = True
-      print("deck length:", len(deck))
-      for card in deck:
-        print("card:", card)
-        print("card values:", card.value, new_card.value)
-        print("card suits:", card.suit, new_card.suit)
-        if card.value == new_card.value and card.suit == new_card.suit:
-          print("not unique")
-          unique = False
-      while not unique:
-        print("going through not unique loop")
-        # print(3)
-        new_card = Card()
-        unique = True
-        for card in deck:
-        #   print(4)
-          if card.value == new_card.value and card.suit == new_card.suit:
-            unique = False
-      deck.append(new_card)
-    return deck
-    
-  def print_deck(self):
-    for card in self.deck:
-        print(card, end="\n")
-  
-  def get_top_card(self):
-      return self.deck.pop()
+  def draw(self, x: int, y: int, face_down: bool = False):
+    kandinsky.fill_rect(x, y, Card.CARD_OUTSIDE_WIDTH, Card.CARD_OUTSIDE_HEIGHT, Card.CARD_OUTSIDE_COLOR)
+    kandinsky.fill_rect(
+      x + (Card.CARD_OUTSIDE_WIDTH-Card.CARD_INSIDE_WIDTH)//2,
+      y + (Card.CARD_OUTSIDE_HEIGHT-Card.CARD_INSIDE_HEIGHT)//2,
+      Card.CARD_INSIDE_WIDTH,
+      Card.CARD_INSIDE_HEIGHT,
+      Card.CARD_INSIDE_COLOR
+    )
+    if not face_down:
+      if self.get_value_str() == "10":
+        kandinsky.draw_string(self.get_value_str(), x+15, y+20, Card.CARD_OUTSIDE_COLOR)
+      else:
+        kandinsky.draw_string(self.get_value_str(), x+20, y+20, Card.CARD_OUTSIDE_COLOR)
+      # kandinsky.draw_string(self.suit[0], x+20, y+Card.CARD_INSIDE_HEIGHT-30, Card.CARD_OUTSIDE_COLOR)
 
-print("creating game...")
+class Game:
+  def __init__(self):
+    self.deck = Game.generate_deck()
+    self.player_hand = [self.remove_random_card(), self.remove_random_card()]
+    self.dealer_hand = [self.remove_random_card(), self.remove_random_card()]
+
+  @staticmethod
+  def generate_deck():
+    deck = []
+    for value in range(1, 14):
+      for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+        deck.append(Card(value, suit))
+    return deck
+
+  def remove_random_card(self) -> Card:
+    return self.deck.pop(random.randint(0, len(self.deck)-1))
+
+  def hit_or_stay(self) -> bool:
+    if Game.get_sum(self.dealer_hand) < 17:
+      self.dealer_hand.append(self.remove_random_card())
+      return True
+    return False
+
+  def hit(self) -> None:
+    self.player_hand.append(self.remove_random_card())
+
+  @staticmethod
+  def get_sum(hand) -> int:
+    sum = 0
+    aces = 0
+    for i in range(len(hand)):
+      card = hand[i]
+      if card.value == 1 and aces == 0:
+        aces += 1
+        sum += 11
+      elif card.value == 11 or card.value == 12 or card.value == 13:
+        sum += 10
+      else:
+        sum += card.value
+    if sum > 21:
+      sum = 0
+      for i in range(len(hand)):
+        card = hand[i]
+        sum += card.value
+    return sum
+
+  def print_deck(self) -> None:
+    for card in self.deck:
+      print(card)
+
+  def get_dealer_hand_str(self) -> None:
+    string = ""
+    for card in self.dealer_hand:
+      string += str(card) + ", "
+    return string
+
+  def get_player_hand_str(self) -> None:
+    string = ""
+    for card in self.player_hand:
+      string += str(card) + ", "
+    return string
+
 game = Game()
-print("printing deck...")
-game.print_deck()
-print("printing top card...")
-print(game.get_top_card())
+draw = True
+win = None
+face_up = False
+while True:
+  if draw:
+    draw = False
+    for idx, card in enumerate(game.dealer_hand):
+      if idx == 0 and not face_up:
+        card.draw((idx+1)*10 + Card.CARD_OUTSIDE_WIDTH*idx, 10, True)
+      else:
+        card.draw((idx+1)*10 + Card.CARD_OUTSIDE_WIDTH*idx, 10)
+    for idx, card in enumerate(game.player_hand):
+      card.draw((idx+1)*10 + Card.CARD_OUTSIDE_WIDTH*idx, HEIGHT-10-Card.CARD_OUTSIDE_HEIGHT)
+  if ion.keydown(ion.KEY_DOWN):
+    face_up = True
+    if win is not None:
+      continue
+    draw = True
+    while game.hit_or_stay():
+      game.hit_or_stay()
+    if Game.get_sum(game.dealer_hand) > 21:
+      win = True
+    elif Game.get_sum(game.dealer_hand) >= Game.get_sum(game.player_hand):
+      win = False
+  elif ion.keydown(ion.KEY_UP) and not hitting:
+    hitting = True
+    if win is not None:
+      continue
+    draw = True
+    game.hit()
+    if Game.get_sum(game.player_hand) > 21:
+      win = False
+  if not ion.keydown(ion.KEY_UP):
+    hitting = False
+  if not draw and win is not None:
+    break
+
+print(win)
